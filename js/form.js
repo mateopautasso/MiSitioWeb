82b2b02b95f66e4aed3e1516cad2e99f4644ad18
mateopautassoPC <mateopautasso.mg@gmail.com>
2023-02-25 18:10:58 -0300

    Section contacto finalizada. Scroll al apretar las anclas de header agregado

--- js/form.js
+++ js/form.js
@@ -1,5 +1,48 @@
+const form = document.querySelector('.form');
+const campoName = document.getElementById('name')
+const campoEmail = document.getElementById('email')
+const campoMsg = document.getElementById('msg')
 const submit = document.querySelector('.form__input-submit');
+const linkEmail = document.getElementById('linkEmail')
+const errorP = document.querySelector('.contacto__form-container p');
 
-// submit.addEventListener('click', (e)=>{
-//     e.preventDefault()
-// })
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+}
+const objValidacion = {
+    name: false,
+    email: false,
+}
+
+
+campoName.addEventListener('keyup',()=>{
+    if(expresiones.nombre.test(campoName.value)) {
+        objValidacion.name = true;
+    } else {
+        objValidacion.name = false;
+    }
+})
+campoEmail.addEventListener('keyup',()=>{
+    if(expresiones.correo.test(campoEmail.value)) {
+        objValidacion.email = true;
+    } else {
+        objValidacion.email = false;
+    }
+})
+
+
+submit.addEventListener('click',(e)=>{
+    e.preventDefault()
+    if(objValidacion.name === true && objValidacion.email === true && campoMsg.value !== '') {
+        let name = campoName.value;
+        let email = campoEmail.value;
+        let msg = campoMsg.value;
+        linkEmail.setAttribute('href', `mailto:mateopautassodev@gmail.com?subject=Soy%20${name}%20y%20lo%20contacto%20desde%20su%20sitio%20web%20-%20Correo:%20${email}&body=${msg}`);
+        linkEmail.click();
+        errorP.innerHTML = ''
+    } else {
+        errorP.innerHTML = 'Campos incompletos!'
+    }
+})
--- js/main.js
+++ js/main.js
@@ -11,3 +11,48 @@ window.addEventListener('scroll', ()=>{
         header.classList.remove("header-inactive")
     }
 })
+
+
+// Scroll a section contacto
+const aContacto = document.getElementById('contacto-section')
+const aSkills = document.getElementById('skills-section')
+const aPracticas = document.getElementById('practicas-section')
+const aProyectos = document.getElementById('proyectos-section')
+const aHeader = document.querySelectorAll('.a-header');
+
+aContacto.addEventListener('click', (e)=>{
+    e.preventDefault()
+    scroll(0,2250)
+})
+
+aHeader.forEach((a)=>{
+    a.addEventListener('click',(e)=>{
+        e.preventDefault();
+        let aClickeada = e.target;
+        switch(aClickeada.id) {
+            case "skills-section":
+                console.log('stack');
+                scroll(0,780);
+                break;
+            case "practicas-section":
+                console.log('Practicas');
+                scroll(0,1520);
+                break;
+            case "proyectos-section":
+                console.log('Practicas');
+                scroll(0,0);
+                // Falta la seccion
+                break;
+            case "sobremi-section":
+                console.log('Practicas');
+                scroll(0,0);
+                // Falta la seccion
+                break;
+            case "contacto-section":    
+                scroll(0,2250);
+                break;
+            default:
+                console.log('Error')
+        }
+    })
+})
