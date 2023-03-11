0d3f82bdf082e3180426e4cfa34c9d4b6c76b955
mateopautassoPC <mateopautasso.mg@gmail.com>
2023-03-11 15:32:47 -0300

    card 'Form profesional' agregada

--- js/cards-proyectos.js
+++ js/cards-proyectos.js
@@ -23,6 +23,7 @@ const cards = []
 function crearCard (url,title,tech,link) {
     cards.push(newCard = new CardProyectos(url,title,tech,link))
 }
+crearCard('formProfesional.png','Validación de Formulario','Html, Css, Javascript','https://mateopautasso.github.io/FormularioProfesional/')
 crearCard('temporizador.png','Temporizador','Html, Css, Javascript','https://mateopautasso.github.io/Temporizador/')
 crearCard('calculadora.png','Calculadora con JS','Html, Css, Javascript','https://mateopautasso.github.io/Calculadora/')
 crearCard('jnSpecter.png','Specter | Codewell Challenge','Html, Css','https://mateopautasso.github.io/JohnathanSpecter-web/')
